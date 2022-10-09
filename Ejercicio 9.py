f074ce6889fd04d5de7cd9c863243d71ab3f9758
Gabteh <115422844+Gabteh@users.noreply.github.com>
2022-10-09 18:42:49 -0300

    Add files via upload

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,3 +1,9 @@
 Lado_1 = float(input("ingrese la primera nota"))
 Lado_2 = float(input("ingrese la segunda nota"))
 Lado_3 = float(input("ingrese la tercera nota"))
+if Lado_1 == Lado_2 and Lado_1 == Lado_3 and Lado_2==Lado_3:
+    print("Es un triangulo equilatero")
+elif Lado_1 == Lado_2 and Lado_1 != Lado_3 and Lado_2 != Lado_3:
+    print("Es un triangulo isoceles")
+elif Lado_1 != Lado_2 and Lado_1 != Lado_3 and Lado_2 != Lado_3:
+    print ("Es  un triangulo escaleno")
